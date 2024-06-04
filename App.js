b666f69ff95bc597147066faf5f324231464aeba
Ethan Anderson <et714492@dal.ca>
2024-06-03 23:21:13 -0300

    editing and stuff

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { useEffect, useLayoutEffect } from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { TouchableOpacity, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import MasonryList from '@react-native-seoul/masonry-list'
-import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation } from './db';
+import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation, useUpdateNoteMutation } from './db';
 
 function HomeScreen({ navigation }) {
   const { data: searchData, error, isLoading } = useSearchNotesQuery("");
@@ -21,17 +21,17 @@ function HomeScreen({ navigation }) {
   }, [addNoteData]);
 
   const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => deleteNote(item) } style={tw`w-[98%] mb-0.5 mx-auto bg-purple-300 rounded-sm px-1`}> 
-      <Text style={tw`text-xl mb-1`}>{item.title}</Text>
-      <Text>{item.content}</Text>
+    <TouchableOpacity onPress={() => deleteNote(item) } style={tw`w-[98%] mb-0.5 mx-auto bg-gray-800 rounded-sm px-1`}> 
+      <Text style={tw`text-white text-2xl m-3`}>{item.title}</Text>
+      <Text style={tw`text-white m-3 mb-5`}>{item.content}</Text>
     </TouchableOpacity>
   )
 
   return (
-    <View style={tw`flex-1 items-center justify-center bg-purple-400`}>
+    <View style={tw`flex-1 items-center justify-center bg-gray-900`}>
       {searchData ? 
         <MasonryList
-          style={tw`px-0.5 pt-0.5 pb-20`}
+          style={tw`p-5 pb-20`}
           data={searchData}
           numColumns={2}
           renderItem={renderItem}
@@ -48,13 +48,41 @@ function HomeScreen({ navigation }) {
 }
 
 function EditScreen({ route, navigation }) {
+  const [ addNote, { data: addNoteData, error: addNoteError }] = useAddNoteMutation();
+  const [ updateNote, { data: updateNoteData, error: updateNoteError }] = useUpdateNoteMutation();
+
+  const [titleTextValue, setTitleTextValue] = useState(route.params.data.title); 
+  const [contentTextValue, setContentTextValue] = useState(route.params.data.content); 
+
+  const handleTextClick = () => {
+  };
+
+  const handleSubmitEditing = () => {
+    console.log("submitting");
+    console.log(titleTextValue);
+    console.log(contentTextValue);
+    updateNote({id: route.params.data.id, title: titleTextValue, content: contentTextValue});
+  };
+
   useLayoutEffect(() => {
+    //addNote({title: titleTextValue, content: contentTextValue});
     navigation.setOptions({ title: route.params.data.title });
   }, []);
 
   return (
-    <View style={tw`flex-1 items-center justify-center bg-purple-400`}>
-      <Text style={tw`text-lg text-white`}>Edit Screen {route.params.data.title} {route.params.data.content}</Text>
+    <View style={tw`flex-1 items-center justify-center bg-gray-900`}>
+      <TextInput style={tw`text-white`}
+          value={titleTextValue}
+          onChangeText={setTitleTextValue}
+          onSubmitEditing={handleSubmitEditing}
+          autoFocus={true}
+        />
+      <TextInput style={tw`text-white`}
+          value={contentTextValue}
+          onChangeText={setContentTextValue}
+          onSubmitEditing={handleSubmitEditing}
+          autoFocus={true}
+        />
     </View>
   );
 }
@@ -70,7 +98,7 @@ export default function App() {
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen
             options={{
-              headerStyle: tw`bg-purple-300 border-0`,
+              headerStyle: tw`bg-gray-800 border-0`,
               headerTintColor: '#fff',
               headerTitleStyle: tw`font-bold`,
               headerShadowVisible: false, // gets rid of border on device
@@ -80,7 +108,7 @@ export default function App() {
           />
           <Stack.Screen
             options={{
-              headerStyle: tw`bg-purple-300 border-0`,
+              headerStyle: tw`bg-gray-800 border-0`,
               headerTintColor: '#fff',
               headerTitleStyle: tw`font-bold`,
               headerShadowVisible: false, // gets rid of border on device
